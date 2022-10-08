486a7f023057e29456a337266a65ae7112a811b7
hoangdaidev <hoangdaideveloper@gmail.com>
2022-10-08 09:56:04 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const cors = require('cors');
+const contactsRouter = require("./app/routes/contact.route")
 
 const app = express();
+
 
 app.use(cors());
 app.use(express.json());
@@ -9,5 +11,7 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.json({ message: "wellcome to contact book application"});
 });
+
+app.use("/api/contacts", contactsRouter);
 
 module.exports = app;
